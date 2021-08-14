99f6c7f2dfd84464217da50793f816a910aec3ad
Fernando Gomez <fernando.gomez1401@gmail.com>
2021-08-13 19:08:59 -0600

    Init -> dev Use Data Query

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from '@dhis2/app-runtime'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import Home from './Home';
+
+const appConfig = {
+  baseUrl: 'http://localhost:3020/https://play.dhis2.org/dev',
+  apiVersion: 33,
 }
+
+const App = () => (
+  <Provider config={appConfig}>
+    <Home></Home>
+  </Provider>
+)
+
 
 export default App;
